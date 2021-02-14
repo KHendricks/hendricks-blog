354a7585e27a23100af4ccfc04a6a340e825ff3e
Kyle Hendricks <kylehendricks0707@gmail.com>
2021-02-13 22:31:51 -0500

    Retweaked ui to use flex boxes, added shading to map, added onclick when a state is pressed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// Material UI
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import PhotoIcon from "@material-ui/icons/Photo";
+import Box from "@material-ui/core/Box";
+
+// Simple Maps
+import MapChart from "./components/MapChart";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Provider store={store}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        p={7}
+        m={1}
+        // style={{ background: "red" }}
+      >
+        <Box
+          display="flex"
+          alignItems="flex-end"
+          flexDirection="column"
+          // style={{ background: "blue" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon style={{ fontSize: 48 }} />
+          </IconButton>
+        </Box>
+
+        <Box
+          display="flex"
+          alignItems="center"
+          p={1}
+          m={1}
+          flexDirection="row"
+          justifyContent="space-around"
+          // style={{ background: "green" }}
+        >
+          <Box p={1}>
+            <Typography variant="h1" style={styles.welcomeStyle}>
+              Welcome to the T.H.F Blog
+            </Typography>
+          </Box>
+          {/* <Box p={1}>
+            <PhotoIcon style={{ fontSize: 100 }} />
+          </Box> */}
+        </Box>
+
+        <Box
+          display="flex"
+          alignItems="center"
+          p={10}
+          m={1}
+          flexDirection="row"
+          justifyContent="space-around"
+          // style={{ background: "pink" }}
+        >
+          <Typography
+            variant="h6"
+            align="center"
+            style={styles.introductionTextStyle}
+          >
+            Welcome to our blog! Our names are Kyle and Heather, and we are the
+            Hendricks family! We have traveled around the USA for work trips and
+            vacations, and we always try to find the hidden treasures our world
+            has to offer, from small local restaurants to amazing natural
+            landmarks.
+          </Typography>
+        </Box>
+
+        <Box
+          display="flex"
+          alignItems="center"
+          p={1}
+          m={1}
+          flexDirection="row"
+          justifyContent="space-around"
+          // style={{ background: "pink" }}
+        >
+          <MapChart />
+        </Box>
+      </Box>
+    </Provider>
   );
-}
+};
+
+const styles = {
+  containerBox: {},
+  toolbarStyle: {
+    justifyContent: "flex-end",
+  },
+  appBarStyle: { color: "black", background: "transparent", boxShadow: "none" },
+  welcomeStyle: {
+    color: "black",
+    fontFamily: "Alegreya",
+  },
+  introductionTextStyle: {
+    color: "black",
+    fontFamily: "Ariel",
+  },
+};
 
 export default App;
---
+++ src/components/MapChart.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { geoCentroid } from "d3-geo";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation,
+} from "react-simple-maps";
+import { PatternLines } from "@vx/pattern";
+
+import allStates from "./data/allstates.json";
+import statesData from "./data/states.json";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+
+const geoJson = statesData;
+
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+};
+
+const highlighted = [
+  "Florida",
+  "California",
+  "Texas",
+  "Alaska",
+  "Colorado",
+  "Virginia",
+  "Washington",
+];
+
+const MapChart = () => {
+  return (
+    <ComposableMap
+      style={{ height: 1000, width: 1000 }}
+      projection="geoAlbersUsa"
+    >
+      <PatternLines
+        id="lines"
+        height={6}
+        width={6}
+        stroke="#5bd785"
+        strokeWidth={2}
+        background="#FFF"
+        orientation={["vertical"]}
+      />
+      <Geographies geography={geoJson}>
+        {({ geographies }) => (
+          <>
+            {geographies.map((geo) => {
+              return (
+                <>
+                  <Geography
+                    key={geo.properties}
+                    stroke="#001440"
+                    geography={geo}
+                    fill={
+                      highlighted.includes(geo.properties.name)
+                        ? "url('#lines')"
+                        : "#99e6b3"
+                    }
+                    onClick={() =>
+                      console.log("clicked: ", geo.properties.name)
+                    }
+                  />
+                </>
+              );
+            })}
+            {geographies.map((geo) => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find((s) => s.val === geo.id);
+              return (
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text x={4} fontSize={14} alignmentBaseline="middle">
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              );
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default MapChart;
